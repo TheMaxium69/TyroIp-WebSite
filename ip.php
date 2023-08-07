d53bd204488cc0e1dcca16ffdd6d192f4642fb8f
Maxime Tournier <mxm.tournier@gmail.com>
2023-08-07 20:09:57 +0200

    update

---
+++ ip.php
@@ -0,0 +1,28 @@
+<?php $ip = $_SERVER['REMOTE_ADDR']; ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<head>
+    <title>ip.tyrolium.fr</title>
+    <META http-equiv="Content-type" content="text/html; charset=UTF-8">
+</head>
+<P ALIGN="center"><FONT size=8>
+	<BR>IP : <?= $ip ?> <br>
+</font>
+
+<font size=1><br><br>By <a href="https://tyrolium.fr">Tyrolium.fr</font></html>
+
+<style>
+
+body {
+
+	background: #383838;
+	color : white;
+}
+
+a {
+	color: white;
+}
+
+</style>
